a63d73aa14916c137b855bf7390e7873950635fa
Pawel Kaczanowski <pawel.kaczanowski@smtsoftware.com>
2014-05-22 16:04:02 +0200

    dodanie mapowania na przekierowania do epuap

--- spring-mcv-with-camel/src/main/java/com/smt/example/camel/home/HomeController.java
+++ spring-mcv-with-camel/src/main/java/com/smt/example/camel/home/HomeController.java
@@ -3,13 +3,21 @@ package com.smt.example.camel.home;
 import java.security.Principal;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
-	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String index(Principal principal) {
-		return principal != null ? "home/homeSignedIn" : "home/homeNotSignedIn";
-	}
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(Principal principal) {
+        return principal != null ? "home/homeSignedIn" : "home/homeNotSignedIn";
+    }
+
+    //    @RequestMapping("epuap")
+    public String epuap() {
+
+        return "redirect:https://hetman.epuap.gov.pl/DracoEngine2/draco.jsf";
+
+    }
 }
